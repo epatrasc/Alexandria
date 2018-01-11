d6e02ae6db2e9fd86d346702c7d5553c889067cf
Emilian Patrasc <emilian.patrasc@edu.unito.it>
2018-01-11 07:07:16 +0000

    add android request handler

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -12,29 +13,50 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.implement.UtenteDaoImpl;
+import model.StatusResponse;
 import model.Utente;
+import utils.JSONManager;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = -1389065591634304311L;
-
+	private JSONManager JSONMan = new JSONManager();
+	
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		ServletContext ctx = getServletContext();
 
 		String nome = request.getParameter("nome");
 		String password = request.getParameter("password");
-
+		boolean isAndroid = getIsAndroid(request.getParameter("isAndroid"));
+		
 		if (nome == null || password == null) {
+			if(isAndroid){
+				response.setContentType("application/json");
+				PrintWriter out = response.getWriter();
+				out.append(JSONMan.serializeJson(new StatusResponse(false, String.format("Parametri input non validi"))));
+				out.close();
+				return;
+			}
+			
 			RequestDispatcher rd = ctx.getRequestDispatcher("/login.jsp");
 			rd.forward(request, response);
 			return;
 		}
-
+		
+		
 		Utente utente = new UtenteDaoImpl(nome, password).getUtente();
 
-		// login fallito, mando messaggio alla jsp
+		// login fallito, utente o password non validi
 		if (utente == null) {
+			if(isAndroid){
+				response.setContentType("application/json");
+				PrintWriter out = response.getWriter();
+				out.append(JSONMan.serializeJson(new StatusResponse(false, String.format("Utente o password errati. Riprovare"))));
+				out.close();
+				return;
+			}
+			
 			request.setAttribute("loginError", true);
 			request.setAttribute("messaggio", "Utente o password errati. Riprovare");
 
@@ -43,11 +65,24 @@ public class LoginController extends HttpServlet {
 			return;
 		}
 
-		// login ok, redirigo ad home
+		// login ok, se android mando json se no redirigo su home
 		HttpSession session = request.getSession();
 		session.setAttribute("isLogged", true);
 		session.setAttribute("utente", utente);
+		
+		if(isAndroid){
+			response.setContentType("application/json");
+			PrintWriter out = response.getWriter();
+			out.append(JSONMan.serializeJson(new StatusResponse(true, String.format("Login ok"))));
+			out.close();
+			return;
+		}
+		
 		response.sendRedirect(request.getContextPath() + "/home");
+	}
+
+	private boolean getIsAndroid(String input) {
+		return input != null ? Boolean.parseBoolean(input) : false;
 	}
 
 	@Override
